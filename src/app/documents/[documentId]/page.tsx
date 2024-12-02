46b0868b1d954690bc19282cd79cd389ce999383
Pratik <93414233+pratik-git10@users.noreply.github.com>
2024-12-02 15:47:40 +0530

    Added Shadcn Components and Documents File With editor

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,19 @@
+import { Editor } from "./editor";
+import { ToolBar } from "./toolbar";
+
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: string }>;
+}
+
+const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
+
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <ToolBar />
+      <Editor />
+    </div>
+  );
+};
+
+export default DocumentIdPage;
